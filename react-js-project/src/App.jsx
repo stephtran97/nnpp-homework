9904e2846d53b63c5cb2c6edd96dcd6a1171d925
ThinhTD <tranducthinh.97@gmail.com>
2024-09-30 01:49:26 +0700

    homework: exercises 2 ex1

--- react-js-project/src/App.jsx
+++ react-js-project/src/App.jsx
@@ -1,5 +1,34 @@
-function App() {
-  return <h1>Override your content here</h1>;
-}
+import React, { useState } from 'react';
 
-export default App;
+const tabs = ['My Account', 'Company', 'Team Members', 'Billing'];
+
+// Update the current tab (to toggle its appearance when clicked) using the state setter function
+export default function Tabs() {
+  const [currentTab, setCurrentTab] = useState('My Account');
+  // let currentTab = "My Account";
+
+  return (
+    <div className='flex justify-center my-8'>
+      <div
+        className='flex space-x-4'
+        aria-label='Tabs'
+      >
+        {tabs.map((tab) => (
+          <button
+            key={tab}
+            onClick={() => {
+              setCurrentTab(tab);
+            }}
+            className={`${
+              tab === currentTab
+                ? 'bg-indigo-100 text-indigo-700'
+                : 'text-gray-500 hover:text-gray-700'
+            } rounded-md px-3 py-2 text-sm font-medium`}
+          >
+            {tab}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
