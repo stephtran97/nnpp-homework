0e141657116d2e2a0d493c316d92a690aa11c5bf
ThinhTD <tranducthinh.97@gmail.com>
2024-09-30 00:20:05 +0700

    homework: init coin homework

--- react-js-project/src/App.jsx
+++ react-js-project/src/App.jsx
@@ -1,5 +1,23 @@
+import React from 'react';
+
+import BigCoin from './BigCoin';
+
 function App() {
-  return <h1>Override your content here</h1>;
+  const [coins, setCoins] = React.useState(0);
+  const addCoinsHandler = (coin) => {
+    setCoins(coin);
+  };
+  return (
+    <div className='wrapper'>
+      <main>
+        <BigCoin onCoinHandler={addCoinsHandler} />
+      </main>
+      <footer>
+        Your coin balance:
+        <strong>{coins}</strong>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
