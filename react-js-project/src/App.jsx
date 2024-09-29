f73745a543d29bbc5110b6b12e17a7cbc2c3bef1
ThinhTD <tranducthinh.97@gmail.com>
2024-09-30 02:28:30 +0700

    homework: exercises 2 ex5

--- react-js-project/src/App.jsx
+++ react-js-project/src/App.jsx
@@ -1,5 +1,82 @@
-function App() {
-  return <h1>Override your content here</h1>;
+import { MinusSmallIcon, PlusSmallIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
+
+const faqs = [
+  {
+    question: "What's the best thing about Switzerland?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat."
+  },
+  {
+    question: 'What do you call someone with no body and no nose?',
+    answer:
+      'Nobody knows. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, voluptas ipsa quia excepturi, quibusdam natus exercitationem sapiente tempore labore voluptatem.'
+  }
+];
+
+// Share state by lifting the `isOpen` state variable to the parent component (FAQ) and passing data down via props
+export default function FAQ() {
+  const [selectedIndex, setSelectedIndex] = useState();
+  return (
+    <div className='bg-white'>
+      <div className='mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40'>
+        <div className='mx-auto max-w-4xl divide-y divide-gray-900/10'>
+          <h2 className='text-2xl font-bold leading-10 tracking-tight text-gray-900'>
+            Frequently asked questions
+          </h2>
+          <dl className='mt-10 space-y-6 divide-y divide-gray-900/10'>
+            {faqs.map((faq, index) => (
+              <Accordion
+                key={index}
+                faq={faq}
+                isOpen={selectedIndex === index}
+                onSelect={() => {
+                  if (selectedIndex === index) {
+                    setSelectedIndex(null);
+                    return;
+                  }
+                  setSelectedIndex(index);
+                }}
+              />
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App;
+function Accordion({ faq, isOpen, onSelect }) {
+  return (
+    <div className='pt-6'>
+      <dt>
+        <button
+          onClick={() => onSelect()}
+          className='flex w-full items-start justify-between text-left text-gray-900'
+        >
+          <span className='text-base font-semibold leading-7'>
+            {faq.question}
+          </span>
+          <span className='ml-6 flex h-7 items-center'>
+            {isOpen ? (
+              <MinusSmallIcon
+                className='h-6 w-6'
+                aria-hidden='true'
+              />
+            ) : (
+              <PlusSmallIcon
+                className='h-6 w-6'
+                aria-hidden='true'
+              />
+            )}
+          </span>
+        </button>
+      </dt>
+      {isOpen && (
+        <dd className='mt-2 pr-12'>
+          <p className='text-base leading-7 text-gray-600'>{faq.answer}</p>
+        </dd>
+      )}
+    </div>
+  );
+}
