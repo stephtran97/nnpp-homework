8db287eb8b931f1b3a93385ab0c0edcd4e9bab20
ThinhTD <tranducthinh.97@gmail.com>
2024-09-30 01:31:52 +0700

    homework: exercises 1 ex5

--- react-js-project/src/App.jsx
+++ react-js-project/src/App.jsx
@@ -1,53 +1,11 @@
-import { StarIcon } from '@heroicons/react/20/solid';
+import Header from './Header';
+import Form from './Form';
 
-const review = {
-  rating: 4,
-  content:
-    "I really loved this product, but then I took it out of the box and realized I didn't like it at all.",
-  author: 'Emily Selman',
-  avatarSrc:
-    'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80'
-};
-
-// Good review has rating >= 4
-// If no author, hide entire Review
-// If good review, show ReviewRating, otherwise hide it. In place of review content, say "No review".
-export default function GoodReview() {
-  const isGoodReview = review.rating >= 4;
-  if (!review.author) return null;
-  return review.rating >= 4 ? (
-    <div className='review'>
-      <div className='review-wrapper'>
-        <img
-          className='review-avatar'
-          src={review.avatarSrc}
-        />
-        <div className='review-left'>
-          <h4 className='review-author'>{review.author}</h4>
-          {isGoodReview ? <ReviewRating review={review} /> : null}
-        </div>
-      </div>
-      <div className='review-content'>
-        {isGoodReview ? review.content : 'No review'}
-      </div>
-    </div>
-  ) : null;
-}
-
-function ReviewRating({ review }) {
-  const stars = Array(5).fill(false);
-  for (let i = 0; i < review.rating; i++) {
-    stars[i] = true;
-  }
+export default function App() {
   return (
-    <div className='review-rating'>
-      {stars.map((rating) =>
-        rating ? (
-          <StarIcon className='review-star review-star--filled' />
-        ) : (
-          <StarIcon className='review-star review-star--empty' />
-        )
-      )}
+    <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+      <Header />
+      <Form />
     </div>
   );
 }
